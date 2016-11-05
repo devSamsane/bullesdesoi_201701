7b9e61565672d3d5e1a2cd695411a6f5bb14e292
olivier <lavauxo@gmail.com>
2016-11-05 22:09:58 +0100

    fix(core.authentication) : issue #4

--- modules/users/client/services/users.client.service.js
+++ modules/users/client/services/users.client.service.js
@@ -4,25 +4,88 @@
 (function () {
   'use strict';
 
-  // Users directive used to force lowercase input
+  // Users service used for communicating with the users REST endpoint
   angular
-    .module('users')
-    .directive('lowercase', lowercase);
+    .module('users.services')
+    .factory('UsersService', UsersService);
 
-  function lowercase() {
-    var directive = {
-      require: 'ngModel',
-      link: link
-    };
+  UsersService.$inject = ['$resource'];
 
-    return directive;
+  function UsersService($resource) {
+    var Users = $resource('/api/users', {}, {
+      update: {
+        method: 'PUT'
+      },
+      updatePassword: {
+        method: 'POST',
+        url: '/api/users/password'
+      },
+      deleteProvider: {
+        method: 'DELETE',
+        url: '/api/users/accounts',
+        params: {
+          provider: '@provider'
+        }
+      },
+      sendPasswordResetToken: {
+        method: 'POST',
+        url: '/api/auth/forgot'
+      },
+      resetPasswordWithToken: {
+        method: 'POST',
+        url: '/api/auth/reset/:token'
+      },
+      signup: {
+        method: 'POST',
+        url: '/api/auth/signup'
+      },
+      signin: {
+        method: 'POST',
+        url: '/api/auth/signin'
+      }
+    });
 
-    function link(scope, element, attrs, modelCtrl) {
-      modelCtrl.$parsers.push(function (input) {
+    angular.extend(Users, {
+      changePassword: function (passwordDetails) {
+        return this.updatePassword(passwordDetails).$promise;
+      },
+      removeSocialAccount: function (provider) {
+        return this.deleteProvider({
+          provider: provider // api expects provider as a querystring parameter
+        }).$promise;
+      },
+      requestPasswordReset: function (credentials) {
+        return this.sendPasswordResetToken(credentials).$promise;
+      },
+      resetPassword: function (token, passwordDetails) {
+        return this.resetPasswordWithToken({
+          token: token // api expects token as a parameter (i.e. /:token)
+        }, passwordDetails).$promise;
+      },
+      userSignup: function (credentials) {
+        return this.signup(credentials).$promise;
+      },
+      userSignin: function (credentials) {
+        return this.signin(credentials).$promise;
+      }
+    });
 
-        return input ? input.tolowerCase() : '';
-      });
-      element.css('text-transform', 'lowercase');
-    }
+    return Users;
+  }
+
+  angular
+    .module('users.admin.services')
+    .factory('AdminService', AdminService);
+
+  AdminService.$inject = ['$resource'];
+
+  function AdminService($resource) {
+    return $resource('/api/users/:userId', {
+      userId: '@_id'
+    }, {
+      update: {
+        method: 'PUT'
+      }
+    });
   }
 }());
