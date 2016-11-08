0fededeb332b0a5c24955e744c7af22126ce26a4
olivier <lavauxo@gmail.com>
2016-11-08 21:46:38 +0100

    fix(authentication.view) : issue #18

--- modules/users/client/directives/users.client.directive.js
+++ modules/users/client/directives/users.client.directive.js
@@ -20,7 +20,7 @@
     function link(scope, element, attrs, modelCtrl) {
       modelCtrl.$parsers.push(function (input) {
 
-        return input ? input.tolowerCase() : '';
+        return input ? input.toLowerCase() : '';
       });
       element.css('text-transform', 'lowercase');
     }
